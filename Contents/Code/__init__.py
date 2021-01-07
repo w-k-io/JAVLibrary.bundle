8734cc7ac587fcd99e4ee4024e323ca4e2dd0508
Ke Wang <wangke97@outlook.com>
2021-01-07 20:52:48 +1300

    First commit

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,67 @@
+import os
+from datetime import datetime
+
+from Contents.Code.javlibrary import JAVLibrary
+
+
+class JavLibraryAgent(Agent.Movies):
+    name = "JAVLibrary"
+    languages = [
+        Locale.Language.English,
+        Locale.Language.Japanese,
+        Locale.Language.Chinese
+    ]
+    accepts_from = [
+        'com.plexapp.agents.localmedia'
+    ]
+    contributes_to = [
+        'com.plexapp.agents.none'
+    ]
+
+    def search(self, results, media, lang):
+        # Movie name from folder
+        media_path = media.items[0].parts[0].file
+        folder_path = os.path.dirname(media_path)
+        movie_name = self.get_movie_id_from_folder(folder_path)
+        javlibrary_api = JAVLibrary(Prefs["javlibrary_language"])
+        search_results = javlibrary_api.get_metadata(movie_name)
+        if search_results:
+            for result in search_results:
+                results.Append(
+                    MetadataSearchResult(
+                        id=result[0],
+                        name=media.name,
+                        year=None,
+                        score=result[1],
+                        lang=lang
+                    )
+                )
+
+    def update(self, metadata, media, lang):
+        javlibrary_api = JAVLibrary(Prefs["javlibrary_language"])
+        movie_metadata = javlibrary_api.get_metadata(metadata.id)
+        metadata.title = movie_metadata["title"]
+        metadata.title_sort = movie_metadata["id"]
+        metadata.studio = movie_metadata["studio"]
+        metadata.year = movie_metadata["year"]
+        available_date = datetime.strptime(movie_metadata["originally_available_at"], "%Y-%m-%d").date()
+        metadata.originally_available_at = available_date
+        metadata.rating = movie_metadata["rating"]
+        metadata.duration = movie_metadata["duration"]
+        metadata.directors.clear()
+        for director in metadata["directors"]:
+            metadata.directors.new().name = director
+        metadata.directors.clear()
+        for genre in metadata["genres"]:
+            metadata.genres.add(genre)
+        metadata.roles.clear()
+        for role in metadata["roles"]:
+            metadata.roles.new().name = role
+        metadata.duration = movie_metadata["duration"]
+        return metadata
+
+    @staticmethod
+    def get_movie_name_from_folder(folder_path):
+        folder_split = os.path.normpath(folder_path).split(os.sep)
+        movie_name = folder_split[-1]
+        return movie_name
---
+++ Contents/Code/javlibrary.py
@@ -0,0 +1,109 @@
+import cloudscraper
+from bs4 import BeautifulSoup
+
+
+class JAVLibrary:
+    class Languages:
+        EN = "en"
+        JP = "jp"
+        CN = "cn"
+        TW = "tw"
+
+    __BASE_URL = "http://www.javlibrary.com/"
+    __SEARCH_URL = "/vl_searchbyid.php?keyword="
+    __MOVIE_URL = "/?v="
+    __DMM_POSTER_URL = "http://pics.dmm.co.jp/mono/movie/adult/1{movie_id}/1{movie_id}ps.jpg"
+    __DMM_COVER_URL = "http://pics.dmm.co.jp/mono/movie/adult/1{movie_id}/1{movie_id}pl.jpg"
+
+    def __init__(self, language=Languages.CN):
+        self.language = language
+
+    def __get_search_url(self, keyword):
+        return JAVLibrary.__BASE_URL + self.language + JAVLibrary.__SEARCH_URL + keyword
+
+    def __get_movie_url(self, jav_library_id):
+        return JAVLibrary.__BASE_URL + self.language + JAVLibrary.__MOVIE_URL + jav_library_id
+
+    @staticmethod
+    def __get_poster_url(movie_id):
+        return JAVLibrary.__DMM_POSTER_URL.format(movie_id=movie_id)
+
+    @staticmethod
+    def __get_cover_url(movie_id):
+        return JAVLibrary.__DMM_COVER_URL.format(movie_id=movie_id)
+
+    def get_results(self, keyword):
+        results = []
+        score = 100
+        # Bypass Cloudflare anti-bot page
+        scraper = cloudscraper.create_scraper()
+        # Search for keyword on javlibrary.com
+        web_data = scraper.get(self.__get_search_url(keyword)).content.decode("utf-8")
+        soup = BeautifulSoup(web_data, "html.parser")
+        # Redirected to the search result page
+        if soup.find("div", "videos"):
+            # There is at least one match
+            if soup.find("div", "video"):
+                for video in soup.find_all("div", "video"):
+                    movie_id = video.find("a")["href"][5:]
+                    results.append((movie_id, score))
+                    score -= 1
+            # There is no match
+            else:
+                return None
+        # Redirected to the movie page
+        else:
+            movie_id = soup.find("h3", "post-title").find("a")["href"][7:]
+            results.append((movie_id, score))
+
+    def get_metadata(self, movie_id):
+        # Result dict
+        metadata = {
+            "javlibrary_id": "",
+            "id": "",
+            "title": "",
+            "originally_available_at": "",
+            "duration": "",
+            "rating": "",
+            "studio": "",
+            "roles": [],
+            "posters": [],
+            "directors": [],
+            "year": 2000,
+            "thumbs": [],
+            "genres": [],
+        }
+        # Bypass Cloudflare anti-bot page
+        scraper = cloudscraper.create_scraper()
+        # Search for keyword on javlibrary.com
+        movie_data = scraper.get(self.__get_movie_url(movie_id)).content.decode("utf-8")
+        movie_soup = BeautifulSoup(movie_data, "html.parser")
+        metadata["javlibrary_id"] = movie_id
+        metadata["title"] = movie_soup.find("div", {"id": "video_title"}).find("a").text.strip()
+        for tr in movie_soup.find("div", {"id": "video_info"}).find_all("tr"):
+            tr_header = tr.find_all("td")[0].text
+            tr_text = tr.find_all("td")[1]
+            if tr_header == "识别码:":
+                movie_id = tr_text.text.strip().lower().replace("-", "")
+                metadata["id"] = movie_id
+                metadata["posters"] = [self.__get_poster_url(movie_id)]
+                metadata["thumbs"] = [self.__get_cover_url(movie_id)]
+            if tr_header == "发行日期:":
+                metadata["originally_available_at"] = tr_text.text.strip()
+                metadata["year"] = int(tr_text.text.strip().split("-")[0])
+            if tr_header == "长度:":
+                metadata["duration"] = int(tr_text.find("span", "text").text.strip())
+            if tr_header == "导演:":
+                metadata["directors"] = [tr_text.find("span", "director").text.strip()]
+            if tr_header == "制作商:":
+                metadata["studio"] = tr_text.find("span", "maker").text.strip()
+            if tr_header == "类别:":
+                for genre in tr_text.find_all("span", "genre"):
+                    metadata["genres"].append(genre.text.strip())
+            if tr_header == "演员:":
+                for cast in tr_text.find_all("span", "cast"):
+                    metadata["roles"].append(cast.text.strip())
+            if tr_header == "使用者評價:":
+                metadata["rating"] = tr_text.find("span", "score").text.strip("()")
+        print(metadata)
+        return metadata
